eef19f07ac2452cde601bcf526cebd47f14674c8
Andrew Dinsmore <teif26@outlook.com>
2019-02-06 09:22:09 -0500

    Finished using eval, works great, should have read all the instrutions lol

--- calcApp.js
+++ calcApp.js
@@ -1,20 +1,88 @@
-var numBtn = document.getElementsByClassName("calc-button");
+var decimalBtn = document.getElementById("decimal");
+var clearBtn = document.getElementById("clear");
+var operatorBtns = document.querySelectorAll(".operatorBtn");
+var displayValEl = document.getElementById('display')
+var calcNumBtns = document.getElementsByClassName("numBtn");
 
-for (var i=0; i<numBtn.length; i++){           
-  numBtn[i].addEventListener("mouseover", function(){
-    mOverDiv(this)
-   });           
-  numBtn[i].addEventListener("mouseout", function(){
-    mOutDiv(this)
-   });
+var displayVal = "0"
+var pendingVal = "";
+var evalArray = [];
+
+
+  var updateDisplay = (clkObj) =>{
+   btnVal = clkObj.target.innerText;
+    
+   if(displayVal === "0")
+     displayVal = "";
+
+     displayVal += btnVal;
+     displayValEl.innerText = displayVal;
+    
+  }
+
+
+
+for(let i=0;i<calcNumBtns.length;i++){
+    calcNumBtns[i].addEventListener("click", updateDisplay, false);
 }
 
-function mOverDiv(e) {                
-    e.style["z-index"]="3";                
+
+var performOperation = (clkObj) => {
+     operator = clkObj.target.innerText;
+     switch(operator){
+         case "+": 
+         pendingVal = displayVal
+         displayVal = "0"
+         displayValEl.innerText = displayVal;   
+         evalArray.push(pendingVal);
+         evalArray.push("+");
+         break;
+         case "-": 
+         pendingVal = displayVal
+         displayVal = "0"
+         displayValEl.innerText = displayVal;   
+         evalArray.push(pendingVal);
+         evalArray.push("-");
+         break;
+         case "x": 
+         pendingVal = displayVal
+         displayVal = "0"
+         displayValEl.innerText = displayVal;   
+         evalArray.push(pendingVal);
+         evalArray.push("*");
+         break;
+         case "÷": 
+         pendingVal = displayVal
+         displayVal = "0"
+         displayValEl.innerText = displayVal;   
+         evalArray.push(pendingVal);
+         evalArray.push("/");
+         break;
+         case "=":
+         evalArray.push(displayVal)
+         var evaluation = eval(evalArray.join(" "))
+         displayVal = evaluation + '';
+         displayValEl.innerText = displayVal;
+         console.log(evaluation)
+         evalArray = [];
+         default:
+          break;
+         
+         
+        };
+    };
+
+ for (let i =0; i <operatorBtns.length;i++){
+          operatorBtns[i].addEventListener("click", performOperation,false)
+      };
+ 
+clearBtn.onclick = ()=>{
+  displayVal = "0"
+  pendingVal ; 
+  displayValEl.innerText = displayVal;
 }
-
-function mOutDiv(e) {                
-    e.style.background="1";                
+decimalBtn.onclick = ()=>{
+    if(!displayVal.includes("."))
+        displayVal += ".";
+        displayValEl.innerText = displayVal ;
 }
-
-
